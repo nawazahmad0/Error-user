080e56de57e28da92d61a0d9bc6bd3663a89708a
Waring <159174106+nawazahmad0@users.noreply.github.com>
2025-05-30 19:32:20 +0530

    Update trans-Hindi.js

--- Nawaz/commands/trans-Hindi.js
+++ Nawaz/commands/trans-Hindi.js
@@ -1,59 +1,39 @@
 module.exports.config = {
-  name: "hindi",
-  version: "1.0.1",
-  hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
-  description: "Text translation",
-  commandCategory: "media",
-  usages: "[Text]",
-  cooldowns: 5,
-  dependencies: {
-    "request": ""
-  },
-  usePrefix: false
+        name: "hindi",
+        version: "1.0.1",
+        hasPermssion: 0,
+        credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+        description: "Text translation",
+        commandCategory: "media",
+        usages: "[Text]",
+        cooldowns: 5,
+        dependencies: {
+                "request":  ""
+        }
 };
 
-module.exports.handleEvent = async ({ api, event }) => {
-  const request = global.nodemodule["request"];
-  const content = event.body;
+module.exports.run = async ({ api, event, args }) => {
+        const request = global.nodemodule["request"];
+        var content = args.join(" ");
+        if (content.length == 0 && event.type != "message_reply") return global.utils.throwError(this.config.name, event.threadID,event.messageID);
+        var translateThis = content.slice(0, content.indexOf(" ->"));
+        var lang = content.substring(content.indexOf(" -> ") + 4);
+        if (event.type == "message_reply") {
+                translateThis = event.messageReply.body
+                if (content.indexOf("-> ") !== -1) lang = content.substring(content.indexOf("-> ") + 3);
+                else lang = global.config.language;
+        }
+        else if (content.indexOf(" -> ") == -1) {
+                translateThis = content.slice(0, content.length)
+                lang = global.config.language;
+        }
 
-  if (!content && event.type !== "message_reply") return;
-
-  let translateThis, lang;
-
-  if (event.type === "message_reply") {
-    translateThis = event.messageReply.body;
-    if (content.includes("-> ")) {
-      lang = content.substring(content.indexOf("-> ") + 3);
-    } else {
-      lang = global.config.language || "hi";
-    }
-  } else if (content.includes(" -> ")) {
-    translateThis = content.slice(0, content.indexOf(" ->"));
-    lang = content.substring(content.indexOf(" -> ") + 4);
-  } else {
-    return;
-  }
-
-  request(
-    encodeURI(`https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=hi&dt=t&q=${translateThis}`),
-    (err, response, body) => {
-      if (err) return api.sendMessage("❌ Error occurred!", event.threadID, event.messageID);
-
-      try {
-        const retrieve = JSON.parse(body);
-        let text = '';
-        retrieve[0].forEach(item => {
-          if (item[0]) text += item[0];
+        return request(encodeURI(`https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=hi&dt=t&q=${translateThis}`), (err, response, body) => {
+                if (err) return api.sendMessage("An error has occurred!", event.threadID, event.messageID);
+                var retrieve = JSON.parse(body);
+                var text = '';
+                retrieve[0].forEach(item => (item[0]) ? text += item[0] : '');
+                var fromLang = (retrieve[2] === retrieve[8][0][0]) ? retrieve[2] : retrieve[8][0][0]
+                api.sendMessage(` ${text}\n - 🍂🍂 ${fromLang} to 𝐇𝐢𝐧𝐝𝐢🍂🍂`, event.threadID, event.messageID);
         });
-
-        const fromLang = (retrieve[2] === retrieve[8][0][0]) ? retrieve[2] : retrieve[8][0][0];
-        api.sendMessage(` ${text}\n - 🍂🍂 ${fromLang} to 𝐇𝐢𝐧𝐝𝐢🍂🍂`, event.threadID, event.messageID);
-      } catch (e) {
-        return api.sendMessage("⚠️ Translation failed!", event.threadID, event.messageID);
-      }
-    }
-  );
-};
-
-module.exports.run = () => {};
+                                    }
