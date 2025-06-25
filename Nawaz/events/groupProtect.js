8d9b3a21f739693221539360d800987dfb2fb844
Waring <159174106+nawazahmad0@users.noreply.github.com>
2025-06-25 15:27:31 +0530

    Update groupProtect.js

--- Nawaz/events/groupProtect.js
+++ Nawaz/events/groupProtect.js
@@ -5,8 +5,8 @@ const path = __dirname + "/../cache/groupLock.json";
 module.exports.config = {
   name: "antichangeinfobox",
   eventType: ["log:thread-name", "log:thread-image"],
-  version: "1.0.0",
-  credits: "Nawaz Boss"
+  version: "1.0.1",
+  credits: "Nawaz Boss",
 };
 
 module.exports.run = async function ({ api, event }) {
@@ -20,10 +20,12 @@ module.exports.run = async function ({ api, event }) {
 
   try {
     if (event.logMessageType === "log:thread-name") {
+      // console.log("⚠️ Reverting group name...");
       await api.setTitle(name, threadID);
     }
 
     if (event.logMessageType === "log:thread-image" && imageSrc) {
+      // console.log("⚠️ Reverting group image...");
       const res = await axios.get(imageSrc, { responseType: "stream" });
       await api.changeGroupImage(res.data, threadID);
     }
