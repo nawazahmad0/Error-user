eb5497f790fc91507e94d53ad22eb8b6edb9fa32
Waring <159174106+nawazahmad0@users.noreply.github.com>
2025-06-25 14:44:06 +0530

    Update groupProtect.js

--- Nawaz/events/groupProtect.js
+++ Nawaz/events/groupProtect.js
@@ -1,39 +1,42 @@
-const fs = require("fs"); const path = __dirname + "/../../../data/groupProtect.json";
+const fs = require("fs-extra");
+const path = __dirname + "/../../data/groupProtect.json";
 
-// Ensure file exists if (!fs.existsSync(path)) fs.writeFileSync(path, "{}", "utf8");
+module.exports.config = {
+  name: "groupProtect",
+  eventType: ["log:thread-name", "log:thread-image", "log:thread-color", "log:thread-emoji", "log:user-nickname"],
+  version: "1.0.0",
+  credits: "Nawaz Boss",
+  description: "Protects group from unwanted changes",
+  envConfig: {},
+  dependencies: {}
+};
 
-module.exports.config = { name: "groupProtect", eventType: [ "log:thread-name", "log:thread-image", "log:user-nickname", "log:thread-color", "log:thread-emoji" ], version: "1.0", credits: "Nawaz Boss", description: "Block changes to group info" };
+module.exports.run = async function ({ api, event }) {
+  const { threadID, logMessageType } = event;
 
-module.exports.run = async function ({ api, event }) { const threadID = event.threadID; let data;
+  let protectList = {};
+  if (fs.existsSync(path)) protectList = JSON.parse(fs.readFileSync(path));
 
-try { data = JSON.parse(fs.readFileSync(path)); } catch (e) { data = {}; }
+  if (!protectList[threadID]) return;
 
-if (!data[threadID]) return;
+  try {
+    switch (logMessageType) {
+      case "log:thread-name":
+        return api.sendMessage("🚫 Group name change is protected!", threadID);
 
-try { const info = await api.getThreadInfo(threadID); const logType = event.logMessageType;
+      case "log:thread-image":
+        return api.sendMessage("🚫 Group image change is protected!", threadID);
 
-switch (logType) {
-  case "log:thread-name":
-    await api.setTitle(info.threadName, threadID);
-    return api.sendMessage("🚫 Group name change blocked!", threadID);
+      case "log:thread-color":
+        return api.sendMessage("🚫 Group theme is protected!", threadID);
 
-  case "log:thread-image":
-    return api.sendMessage("🚫 Group image change blocked!", threadID);
+      case "log:thread-emoji":
+        return api.sendMessage("🚫 Group emoji is protected!", threadID);
 
-  case "log:user-nickname":
-    const uid = event.logMessageData.participant_id;
-    const oldNick = info.nicknames?.[uid] || "";
-    await api.changeNickname(oldNick, threadID, uid);
-    return api.sendMessage("🚫 Nickname change blocked!", threadID);
-
-  case "log:thread-color":
-    await api.changeThreadColor(info.color, threadID);
-    return api.sendMessage("🚫 Theme color change blocked!", threadID);
-
-  case "log:thread-emoji":
-    await api.changeThreadEmoji(info.emoji, threadID);
-    return api.sendMessage("🚫 Emoji change blocked!", threadID);
-}
-
-} catch (err) { console.error("[GroupProtect Error]:", err); return api.sendMessage("❌ GroupProtect error: " + err.message, threadID); } };
-
+      case "log:user-nickname":
+        return api.sendMessage("🚫 Nickname change is not allowed!", threadID);
+    }
+  } catch (err) {
+    console.error("[GroupProtect Error]", err);
+  }
+};
